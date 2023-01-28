f6bd6883dc21be02a2cd08b21a3a904e38b904b5
RuWebMan <122520940+RuWebMan@users.noreply.github.com>
2023-01-28 23:50:47 +0300

    Типы данных JS

---
+++ app.js
@@ -0,0 +1,33 @@
+// Это однострочный комментарий
+
+/* Это 
+многострочный
+комментарий */
+
+/**
+ * 
+ * Это JSDoc комментарий
+ * 
+ */
+
+/**
+ * Типы данных:
+ * 1. Примитивные
+ * - Number: 2023, 1.5, Nan, Infinity
+ * - String: "Hello", 'Hello', `Hello`
+ * - Boolean: true, false
+ * - NuLL: nuLL
+ * - Undefined: undefined
+ * - Symbol()
+ */
+
+/**
+ * Типы данных:
+ * 2. Объекты(Reference type). Ссылочные типы.
+ * - Object: {name: "Denis", age: 30}
+ * Array: [1, 2, 3, 4, 5]
+ * Function: function foo() {}
+ * Date: new date()
+ */
+
+console.log
