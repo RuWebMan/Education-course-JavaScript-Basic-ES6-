0aba6ab5031daee8a45573e04ed1b1db8323da08
RuWebMan <ruwebman@gmail.com>
2023-01-30 15:50:15 +0300

    Операции с числовыми значениями

--- app.js
+++ app.js
@@ -243,6 +243,54 @@ console.log(typeof value); // boolean
 
 // Числа
 
+const a = 10;
+const b = 20;
+let newValue = a + b;
+console.log(newValue); // 30
+newValue += 10; // прибавляем 10 к value
+console.log(newValue); // 40
+newValue -= 10; // вычитаем 10 от value
+console.log(newValue); // 30
+newValue = 5 % 2; // остаток от деления value на 2
+console.log(newValue); // 1
+value++ // увеличиваем value на 1, увеличение произойдет на следующей строке
+console.log(value); // 2
+value-- // уменьшаем value на 1, уменьшение произойдет на следующей строке
+console.log(value); // 1
+++value // увеличиваем value на 1, увеличение произойдет сразу на этой строке
+console.log(value); // 2
+--value // уменьшаем value на 1, уменьшение произойдет сразу на этой строке
+console.log(value); // 1
+
+// Неточные вычисления
+
+value = 0.6 + 0.7;
+console.log(value); // 1.2999999999999998
+value = +(value.toFixed(1)) // или value = parseFloat(value.toFixed(1)) или value = (0.6 * 10 + 0.7 * 10) / 10
+console.log(value); // 1.3
+
+// Math - объект свойств и методов.
+
+value = Math.PI;
+console.log(value); // 3.141592653589793
+value = Math.E; 
+console.log(value); // 2.718281828459045
+value = Math.random(); // метод random возвращает число от 0 до 1
+value = Math.round(2.4); // метод round округляет число = 2
+value = Math.ceil(2.1); // метод ceil округляет число в большую сторону = 3
+value = Math.floor(2.9); // метод floor округляет число в меньшую сторону = 2
+value = Math.min (10, 20, 30, 40); // метод min возвращает наименьшее число = 10
+value = Math.max (10, 20, 30, 40); // метод max возвращает набольшее число = 40
+
+value = Math.floor(Math.random() * 10 + 1); // метод random возвращает рандомное число от 1 до 10
+console.log(value); // рандомное число от 1 до 10
+
+const arr = ['pink', 'blue', 'green'];
+value = Math.floor(Math.random() * arr.length); // 
+console.log(value, arr[value]); // будет возвращать рандомное значение из массива arr
+
+
+
 
 
 
